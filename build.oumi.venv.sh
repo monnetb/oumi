a558239e2a10a1a8cf34828b08b4cd9d1c4327cf
bruno-monnet <bruno.monnet@hpe.com>
2025-06-06 14:05:48 +0200

    make the venv reflecting the code modifications

--- build.oumi.venv.sh
+++ build.oumi.venv.sh
@@ -7,14 +7,14 @@ export OUMI_GPU=${OUMI_GPU:-NVIDIA}
 export TGT=oumi.venv.${OUMI_GPU}
 
 mkdir -p ${OUMI_ROOT}
-cd ${OUMI_ROOT}
+pushd ${OUMI_ROOT}
 rm -rf ${TGT}
 python -m venv ${TGT}
+popd
 
 source ${OUMI_ROOT}/${TGT}/bin/activate
 
 pip install --upgrade  pip
 
-pip3 install oumi[gpu] vllm
+pip3 install -e .  vllm
 
-#pip3 install vllm
